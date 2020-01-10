884bfb7030f00d91133c4cd8902e0c7b6cd219ec
Hayden <haydos5108@gmail.com>
2020-01-10 22:27:30 +1030

    End of section 7 routing
    lecture 67

---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from 'src/_guards/auth.guard';
+
+/*
+* If any URL paths match the following then the user will be taken to the component
+* The order is important if we had the wild card route first all routes would go there first
+*/
+export const appRouts: Routes = [
+    { path: '', component: HomeComponent },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [ // All children routes in this class are protected by our auth guard see lecture 67
+            { path: 'members', component: MemberListComponent },
+            { path: 'messages', component: MessagesComponent },
+            { path: 'lists', component: ListsComponent }
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+];
